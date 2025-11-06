9d168617a02a19dbf67f1abad06977059923f22f
アクゼスティア <akzestia@gmail.com>
2025-11-07 05:00:38 +0900

    Outline test

--- grammar.js
+++ grammar.js
@@ -49,7 +49,11 @@ module.exports = grammar({
         optional(
           seq(optional(token.immediate(/[ \t]+/)), $.outline_identifier),
         ),
-        optional(token(prec(-1, repeat(choice(/[^*]+/, /\*[^/]/, /\s*.*/))))),
+        optional(
+          token(
+            prec(-1, repeat(choice(/[^*]+/, /\*[^/]/, /[^\/\*]\s*.*[^\*\/]/))),
+          ),
+        ),
         "*/",
       ),
 
